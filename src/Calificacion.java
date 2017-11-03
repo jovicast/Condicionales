21eccbf3959ece607a164bcc60beb114b8e57675
jovicast <myriamcastillo630@gmail.com>
2017-11-02 21:32:06 -0300

    ingresar notas aprobado y reprobado

--- src/Calificacion.java
+++ src/Calificacion.java
@@ -12,17 +12,26 @@ public class Calificacion
 {
    public static void main(String[] args)
     {
-    float nota1, nota2, nota3; 
-    nota1 = Float.parseFloat(JOptionPane.showInputDialog("ingresa un nota"));
-    nota2 = Float.parseFloat(JOptionPane.showInputDialog("ingresa otro nota"));
-    nota3 = Float.parseFloat(JOptionPane.showInputDialog("ingresa otro nota"));
+    float nota1, nota2, nota3, promedio = 0; 
     
-    if(nota1 + nota2 + nota3) //Aqui va una condicion
+    
+    nota1 = Float.parseFloat(JOptionPane.showInputDialog("ingresa un nota1"));
+    nota2 = Float.parseFloat(JOptionPane.showInputDialog("ingresa otro nota2"));
+    nota3 = Float.parseFloat(JOptionPane.showInputDialog("ingresa otro nota3"));
+    promedio= (nota1+nota2+nota3)/3;
+    
+    
+  if(promedio > 4)//Aqui va una condicion
         
     {
+      //Si la condicion se cumple se ejecuta este codigo
+      JOptionPane.showMessageDialog(null, "Aprobado");
+    }   
+     
+  else
+  {
          //Si la condicion se cumple se ejecuta este codigo
-     JOptionPane.showMessageDialog(null, "");   
+     JOptionPane.showMessageDialog(null, "Reprobado"); 
+    }     
     }
-    
-    }  
-}
+    }
